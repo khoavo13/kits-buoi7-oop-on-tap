bd35514beded0172c8bc168f9e3d167239f859a2
khoavo13 <vandangkhoa.vo@gmail.com>
2024-08-28 16:53:14 +0700

    buoi 7 on tap OOP

---
+++ src/CongTyDienLuc/HoaDon.java
@@ -0,0 +1,10 @@
+package CongTyDienLuc;
+
+public class KhachHang {
+    protected String maKH;
+    protected String tenKH;
+    protected int chiSoCu;
+    protected int chiSoMoi;
+    protected long tienThanhToan;
+    
+}
